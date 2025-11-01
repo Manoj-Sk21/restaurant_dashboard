8a9d711438f679e9390bf376648ef83be1d8b772
Manoj-Sk21 <manojsasikumar876@gmail.com>
2025-11-01 12:46:46 +0530

    second commit:selected cuisine and bar chart

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ st.title("My fist real world app")
 all_area=df['Area'].unique()
 selected_area=st.selectbox("select the Area",all_area)
 filterd_df=df[df['Area']==selected_area]
-st.dataframe(filterd_df)
+all_cuisine=filterd_df['Cuisines'].unique()
+selected_cuisine=st.selectbox("select the cuisine",all_cuisine)
+final_df=filterd_df[filterd_df['Cuisines']==selected_cuisine]
+st.bar_chart(final_df['AverageCost'])
+st.dataframe(final_df)
