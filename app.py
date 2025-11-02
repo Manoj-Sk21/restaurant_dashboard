5e53fa8cefd2dddd192e67ff9b49bec7d7107f82
Manoj-Sk21 <manojsasikumar876@gmail.com>
2025-11-02 13:47:23 +0530

    upgraded to full stack api integreation

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import streamlit as st
-df=pd.read_csv("Zomato.csv")
+import requests
+# df=pd.read_csv("Zomato.csv")
 st.set_page_config(layout="wide")
-st.title("My fist real world app")
+st.title("Full stack dashboard")
+API_URL = "https://web-production-b127d.up.railway.app/api/jobs"
+@st.cache_data
+def load_data(url):
+    response = requests.get(url)
+    data = response.json()
+    return pd.DataFrame(data)
+df = load_data(API_URL)
 all_area=df['Area'].unique()
 selected_area=st.selectbox("select the Area",all_area)
 filterd_df=df[df['Area']==selected_area]
